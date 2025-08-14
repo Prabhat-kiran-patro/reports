1aca003a8303bcc43364d2763a7e7943dadddb6f
Prabhat Kiran <prabhatkp2005@gmail.com>
2025-08-14 23:14:11 +0530

    modularise the code

--- create_report.py
+++ create_report.py
@@ -1,25 +1,55 @@
 import json
 import csv
 
-with open("names.json") as file:
-    names = json.load(file)
+class Files_Operator:
 
-with open("scores.json") as file:
-    scores = json.load(file)
+    @staticmethod
+    def fetch_json(file_name):
+        with open(file_name) as file:
+            return json.load(file)
+    
+    @staticmethod
+    def write_to_csv(file_name, data):
+        with open(file_name, "w") as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
 
-absent = [["students with attendance shortage", "", "", "", "", ""]]
-present_scores = [["ID", "NAME", "MATH", "PHY", "CS", "TOTAL"]]
+class Formatter:
 
-for key in names:
-    row = [key, names[key]]
-    if key in scores:
-        row.extend([scores[key]["math"], scores[key]["phy"], scores[key]["cs"], scores[key]["math"]+scores[key]["phy"]+scores[key]["cs"]])
-        present_scores.append(row)
-    else:
-        row.extend(["" for i in range(len(present_scores[0]) - len(row))])
-        absent.append(row)
+    @staticmethod
+    def set_header(arr, *args):
+        arr.append(list(args))
+    
+    @staticmethod
+    def get_scores(val):
+        total = val["math"] + val["phy"] + val["cs"]
+        return [val["math"], val["phy"], val["cs"], total]
 
-with open("report.csv", "w") as file:
-    writer = csv.writer(file)
-    writer.writerows(present_scores)
-    writer.writerows(absent)
+    @staticmethod
+    def to_rows(names, scores, present, absent):
+        for id, name in names.items():
+            row = [id, name]
+            if id not in scores:
+                absent.append(row)
+            else:
+                row.extend(Formatter.get_scores(scores[id]))
+                present.append(row)
+
+def main():
+    # Load data
+    names = Files_Operator.fetch_json("names.json")
+    scores = Files_Operator.fetch_json("scores.json")
+    # Format data
+    present = []
+    absent = []
+    Formatter.set_header(present, "ID", "NAME", "MATH", "PHYSICS", "CS", "TOTAL")
+    Formatter.set_header(absent, "ID", "NAME")
+    Formatter.to_rows(names, scores, present, absent)
+    # Generate reports
+    Files_Operator.write_to_csv("report1.csv", present)
+    Files_Operator.write_to_csv("report2.csv", absent)
+
+    print("Reports generated successfully!")
+
+if __name__ == "__main__":
+    main()
